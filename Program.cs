477d570fab9551d9f065f1a482675fcda6f71106
StarSparkleNova <e.mail1233@yahoo.co.uk>
2023-03-10 21:00:54 +0000

    Well needed update
    Updated

--- Program.cs
+++ Program.cs
@@ -1,64 +1,53 @@
-﻿using System;
+using System;
 
-namespace NumberGuessingGame
+namespace RandomNumberGame
 {
     class Program
     {
-         
-            class Game
-            {
-            private int GeneratedNum; //Private variable for generated number
-            private int NumGuess; //Private variable for User's guess
+        static Random random = new Random();
+        static int maxGuesses = 10;
 
-
-            public Game() //public class for generating random number
-            {
-                Random rnd = new Random(); //initiate random object
-                this.GeneratedNum = rnd.Next(1, 100); //generate random number between 1 and 100
-                this.NumGuess = 0; //Number of guesses variable
-            }
-
-            public string turn(Double UserGuess) //Public function turn, takes UserGuess as input
-            {
-                this.NumGuess++; //Iterate NumGuess
-
-                if (UserGuess > this.GeneratedNum) //if NumGuess is greater than generated number
-                {
-                    return "Your guess is too HIGH!"; //Print too high message
-                }
-                else if (UserGuess < this.GeneratedNum) //if NumGuess is less than generated number
-                {
-                    return "Your guess is too LOW!"; //print guess is too low
-                }
-                else if (UserGuess == this.GeneratedNum) //if it's equal to generated number
-                {
-                    return "Correct! You took " + this.NumGuess + " guesses to find the number!"; //return number of guesses taken
-                }
-
-                return ""; //return nothing from class except in IF statements
-            }
-        }
-
-        static void Main(string[] args) //main method
+        static void Main(string[] args)
         {
+            int randomNumber = GetRandomNumber();
+            int numGuesses = 0;
 
-            Game game = new Game(); //create game object
+            Console.WriteLine("Guess the random number between 1 and 100.");
 
-            Console.WriteLine("A number between 1 and 100 has been generated. Please guess the number!"); //Help text/Instructions
-
-            while (true) //Indefinetly
+            while (numGuesses < maxGuesses)
             {
-                try
-                {
-                    Double UserGuess = Convert.ToDouble(Console.ReadLine()); //Take user guess input
-                    string message = game.turn(UserGuess); //parse UserGuess into Game object
-                    Console.WriteLine(message); //Write message
-                }
-                catch
-                {
-                    Console.WriteLine("Something went wrong"); //Write error message
+                Console.Write("Enter your guess: ");
+                string input = Console.ReadLine();
 
+                if (!int.TryParse(input, out int guess) || guess < 1 || guess > 100)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 100.");
+                    continue;
+                }
+
+                numGuesses++;
+
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine($"Congratulations! You guessed the number in {numGuesses} tries.");
+                    return;
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Too low. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Too high. Try again.");
                 }
             }
+
+            Console.WriteLine($"Sorry, you ran out of guesses. The number was {randomNumber}.");
         }
-    } }
+
+        static int GetRandomNumber()
+        {
+            return random.Next(1, 101);
+        }
+    }
+}
